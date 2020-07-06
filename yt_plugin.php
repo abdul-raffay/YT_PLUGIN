8118d55af88a0fff636b1cf931560bf8bf2ec6a4
abdul-raffay <abdul.raffay@nytrotech.com>
2020-07-06 12:37:27 +0500

    Basic Structure

---
+++ yt_plugin.php
@@ -0,0 +1,12 @@
+<?php 
+/** 
+ * Plugin Name: Youtube Plugin
+ * Description: Youtube Plugin watching tutorials from Traversy Media
+ * Version: 0.1.0
+ * Author: Abdul Raffay
+*/
+
+// Exit if Access Directly by URL by writing path in a URL
+if(!defined('ABSPATH')){
+    exit;
+}

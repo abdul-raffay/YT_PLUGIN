ac895f93db447bd085be4a060d4a1f44d1444bb1
abdul-raffay <abdul.raffay@nytrotech.com>
2020-07-06 13:12:50 +0500

    Widget Initialized

--- yt_plugin.php
+++ yt_plugin.php
@@ -13,3 +13,15 @@ if(!defined('ABSPATH')){
 
 // Load Scripts
 require_once(plugin_dir_path(__FILE__) . '/inc/yt_subs_script.php');
+
+// Load Class
+require_once(plugin_dir_path(__FILE__) . '/inc/yt_subs-class.php');
+
+// Register Widget
+function register_youtube_subs(){
+    // Class Name from class file
+    register_widget('Youtube_Subs_Widget');
+}
+
+// Hook in Function
+add_action('widgets_init', 'register_youtube_subs');

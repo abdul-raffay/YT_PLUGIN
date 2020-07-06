3725186f31a7e4f478df4c48a4c3b9877ed6941b
abdul-raffay <abdul.raffay@nytrotech.com>
2020-07-06 13:49:12 +0500

    Done and Working

--- inc/yt_subs-class.php
+++ inc/yt_subs-class.php
@@ -33,7 +33,8 @@ class Youtube_Subs_Widget extends WP_Widget {
         // Actual Output Content
         // echo esc_html__( 'Hello, World!', 'yts_domain' );
         // echo "Hello from YTS widget";    // for checking
-        echo '<div class="g-ytsubscribe" data-channel="GoogleDevelopers" data-layout="full" data-count="default"></div>';
+            // Youtube Channel
+        echo '<div class="g-ytsubscribe" data-channel="' . $instance['channel'] . '" data-layout="' . $instance['layout'] . '" data-count="' . $instance['count'] . '"></div>';
         
 		echo $args['after_widget'];     // Whatever you want to display after widget(</div>, etc)
 	}
@@ -46,7 +47,13 @@ class Youtube_Subs_Widget extends WP_Widget {
 	 * @param array $instance Previously saved values from database.
 	 */
 	public function form( $instance ) {
-		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'Youtube Subs', 'yts_domain' );
+        $title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'Youtube Subs', 'yts_domain' );
+        
+        $channel = ! empty( $instance['channel'] ) ? $instance['channel'] : esc_html__( 'GoogleDevelopers', 'yts_domain' );
+        
+        $layout = ! empty( $instance['layout'] ) ? $instance['layout'] : esc_html__( 'default', 'yts_domain' );
+
+        $count = ! empty( $instance['count'] ) ? $instance['count'] : esc_html__( 'default', 'yts_domain' );
 		?>
         <!-- TITLE -->
 		<p>
@@ -59,6 +66,49 @@ class Youtube_Subs_Widget extends WP_Widget {
                 name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" 
                 type="text" 
                 value="<?php echo esc_attr( $title ); ?>">
+		</p>
+
+        <!-- CHANNEL NAME -->
+		<p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'channel' ) ); ?>">
+                <?php esc_attr_e( 'Channel:', 'yts_domain' ); ?>
+            </label> 
+            <input 
+                class="widefat" 
+                id="<?php echo esc_attr( $this->get_field_id( 'channel' ) ); ?>" 
+                name="<?php echo esc_attr( $this->get_field_name( 'channel' ) ); ?>" 
+                type="text" 
+                value="<?php echo esc_attr( $channel ); ?>">
+		</p>
+        
+        <!-- CHANNEL Layout -->
+		<p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'layout' ) ); ?>">
+                <?php esc_attr_e( 'Layout:', 'yts_domain' ); ?>
+            </label> 
+            <select 
+                class="widefat" 
+                id="<?php echo esc_attr( $this->get_field_id( 'layout' ) ); ?>" 
+                name="<?php echo esc_attr( $this->get_field_name( 'layout' ) ); ?>" 
+            >
+                <option value="default" <?php echo ($layout == 'default') ? 'selected' : ''; ?>>Default</option>
+                <option value="full" <?php echo ($layout == 'full') ? 'selected' : ''; ?>>Full</option>
+            </select>
+		</p>
+        
+        <!-- CHANNEL Count -->
+		<p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'count' ) ); ?>">
+                <?php esc_attr_e( 'Count:', 'yts_domain' ); ?>
+            </label> 
+            <select 
+                class="widefat" 
+                id="<?php echo esc_attr( $this->get_field_id( 'count' ) ); ?>" 
+                name="<?php echo esc_attr( $this->get_field_name( 'count' ) ); ?>" 
+            >
+                <option value="default" <?php echo ($count == 'default') ? 'selected' : ''; ?>>Default</option>
+                <option value="hidden" <?php echo ($count == 'hidden') ? 'selected' : ''; ?>>Hidden</option>
+            </select>
 		</p>
 		<?php 
 	}
@@ -76,7 +126,13 @@ class Youtube_Subs_Widget extends WP_Widget {
 	public function update( $new_instance, $old_instance ) {
         $instance = array();
         
-		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
+        
+        $instance['channel'] = ( ! empty( $new_instance['channel'] ) ) ? sanitize_text_field( $new_instance['channel'] ) : '';
+
+        $instance['layout'] = ( ! empty( $new_instance['layout'] ) ) ? sanitize_text_field( $new_instance['layout'] ) : 'default';
+
+        $instance['count'] = ( ! empty( $new_instance['count'] ) ) ? sanitize_text_field( $new_instance['count'] ) : 'default';
 
 		return $instance;
 	}

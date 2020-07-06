0ef08ad7af7c730a0907a20875c801202de3157a
abdul-raffay <abdul.raffay@nytrotech.com>
2020-07-06 13:20:22 +0500

    Youtube Subscription API Added

--- inc/yt_subs_script.php
+++ inc/yt_subs_script.php
@@ -6,6 +6,10 @@ function yts_add_scripts(){
     wp_enqueue_style( "yts_main_style", plugins_url() . '/yt_plugin/css/style.css' );
     // Add Main JS
     wp_enqueue_script( "yts_main_script", plugins_url() . '/yt_plugin/js/main.js' );
+
+    // Add Google Scripts
+    wp_register_script( "google", 'https://apis.google.com/js/platform.js' );
+    wp_enqueue_script( "google" );
 }
 
 add_action( "wp_enqueue_scripts", "yts_add_scripts" );
